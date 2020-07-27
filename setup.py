9aaa0670a926c7375be481cddd3777a7cb94a5e5
Vladik Khononov <vladik@khononov.com>
2020-07-27 13:17:01 +0300

    pip 20 fix

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
 from setuptools import setup
-from pip.req import parse_requirements
-
-install_reqs = parse_requirements('requirements.txt', session='hack')
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='json2csv',
       version='2.0.0',
       author='Vladik Khononov',
       packages=['json2csv'],
-      install_requires=reqs,
+      install_requires=['six==1.10.0'],
       entry_points={
           'console_scripts': [
               'json2csv = json2csv.__main__:main'
